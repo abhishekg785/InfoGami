678395fa45cf2431427299b2fbd0dfdaa6f2f575
abhishekg785 <abhishekg785@gmail.com>
2016-09-23 16:28:05 +0530

    search bar bug removed

--- projdir/app/theInfo.py
+++ projdir/app/theInfo.py
@@ -87,8 +87,11 @@ def search_query(request):
         minimized_word_arr = minimize_word_arr(query_word_arr)
         result_tags = TheInfoAddQueryModel.objects.filter(queryTags__name__in = minimized_word_arr).distinct()
         result_ques_text = TheInfoAddQueryModel.objects.filter(queryText__contains = query_text).distinct()
-        minimize_word_arr_query = reduce(operator.and_, (Q(queryText__contains = item) for item in minimized_word_arr))
-        result_minimized_word_arr = TheInfoAddQueryModel.objects.filter(minimize_word_arr_query)
+        try:
+            minimize_word_arr_query = reduce(operator.and_, (Q(queryText__contains = item) for item in minimized_word_arr))
+            result_minimized_word_arr = TheInfoAddQueryModel.objects.filter(minimize_word_arr_query)
+        except:
+            result_minimized_word_arr = []
         result_list = list(chain(result_tags,result_ques_text,result_minimized_word_arr))
         query_result_list = Set(result_list)
         response_arr = []
